a162f0ea5a66475c8bfd91d86178470eafd11233
Brennan Adams <supremecommander757@gmail.com>
2026-04-17 14:57:53 -0400

    Added text toggling logic

--- app/static/js/theme.js
+++ app/static/js/theme.js
@@ -12,3 +12,35 @@ toggle.addEventListener("click", () => {
     root.setAttribute("data-theme", next);
     localStorage.setItem("theme", next);
 });
+
+/* Font Size Toggle Logic */
+const savedSize = localStorage.getItem("font-size");
+if(savedSize){
+    root.style.setProperty("--font-size", savedSize);
+}
+
+const smaller = document.getElementById("text-smaller");
+const bigger = document.getElementById("text-bigger");
+const reset = document.getElementById("text-reset");
+
+function changeSize(delta){
+    const current = parseFloat(getComputedStyle(root).getPropertyValue("--font-size"));
+    const next = current + delta;
+    root.style.setProperty("--font-size", next + "px");
+    localStorage.setItem("font-size", next + "px");
+}
+
+if(smaller){
+    smaller.addEventListener("click", () => changeSize(-2));
+}
+
+if(bigger){
+    bigger.addEventListener("click", () => changeSize(2));
+}
+
+if(reset){
+    reset.addEventListener("click", () => {
+        root.style.setProperty("--font-size", "16px");
+        localStorage.setItem("font-size", "16px");
+    });
+}
